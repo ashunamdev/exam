1ef25a353e94c6ae2c5ce2d3dee65235e1e5bef3
raxit jat <raxitjat2343@gmail.com>
2021-11-30 11:50:29 +0530

    validation add.

--- src/views/Exam/CreateExam.js
+++ src/views/Exam/CreateExam.js
@@ -1,52 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // @material-ui/core components
 // core components
-import GridItem from 'components/Grid/GridItem.js';
-import GridContainer from 'components/Grid/GridContainer.js';
-import Card from 'components/Card/Card.js';
-import CardBody from 'components/Card/CardBody.js';
-import CardHeader from 'components/Card/CardHeader.js';
-import MaterialTable from 'material-table';
-import { makeStyles } from '@material-ui/core/styles';
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Card from "components/Card/Card.js";
+import CardBody from "components/Card/CardBody.js";
+import CardHeader from "components/Card/CardHeader.js";
+import MaterialTable from "material-table";
+import { makeStyles } from "@material-ui/core/styles";
 
-
-import axios from 'axios';
+import axios from "axios";
 
 const styles = {
   typo: {
-    paddingLeft: '25%',
-    marginBottom: '40px',
-    position: 'relative'
+    paddingLeft: "25%",
+    marginBottom: "40px",
+    position: "relative",
   },
   note: {
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-    bottom: '10px',
-    color: '#c0c1c2',
-    display: 'block',
-    fontWeight: '400',
-    fontSize: '13px',
-    lineHeight: '13px',
-    left: '0',
-    marginLeft: '20px',
-    position: 'absolute',
-    width: '260px'
+    bottom: "10px",
+    color: "#c0c1c2",
+    display: "block",
+    fontWeight: "400",
+    fontSize: "13px",
+    lineHeight: "13px",
+    left: "0",
+    marginLeft: "20px",
+    position: "absolute",
+    width: "260px",
   },
   cardCategoryWhite: {
-    color: 'rgba(255,255,255,.62)',
-    margin: '0',
-    fontSize: '14px',
-    marginTop: '0',
-    marginBottom: '0'
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0",
   },
   cardTitleWhite: {
-    color: '#FFFFFF',
-    marginTop: '0px',
-    minHeight: 'auto',
-    fontWeight: '300',
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: '3px',
-    textDecoration: 'none'
-  }
+    marginBottom: "3px",
+    textDecoration: "none",
+  },
 };
 
 const useStyles = makeStyles(styles);
@@ -54,17 +53,17 @@ const useStyles = makeStyles(styles);
 export default function CreateExam() {
   const classes = useStyles();
 
-   const [errors, setErrors] = useState({});
-   const [questionData, setQuestionData] = useState({
-     title: '',
-     assessment_date: '',
-     exam_time: '',
-     duration: '',
-     exam_status: 'upcoming',
-     batch: '',
-     teacher: '',
-     questions: []
-   });
+  const [errors, setErrors] = useState({});
+  const [questionData, setQuestionData] = useState({
+    title: "",
+    assessment_date: "",
+    exam_time: "",
+    duration: "",
+    exam_status: "upcoming",
+    batch: "",
+    teacher: "",
+    questions: [],
+  });
 
   const [questionList, setQuestionList] = useState([]);
   const [selectedQuestion, setSelectedQuestion] = useState([]);
@@ -73,125 +72,123 @@ export default function CreateExam() {
     populateStudentList();
   }, []);
 
-   const handleChangeform = () => {
-     var userData = JSON.parse(window.localStorage.getItem('user'));
-     questionData.questions = selectedQuestion;
-     axios
-       .post('http://3.139.234.205/add-exam/', questionData, {
-         headers: {
-           Authorization: `JWT ` + userData?.token
-         }
-       })
-       .then((res) => {
-         alert(res?.message);
-         console.log('RESPONSE ==== : ', res);
-       })
-       .catch((err) => {
-         alert('something want to wrong');
-         console.log('ERROR: ====', err);
-       });
-     // }
-   };
-   const formErrorValidation = () => {
-     const err = {};
-     if (questionData.username === '') {
-       err.usernameError = 'Username is required.';
-     } else if (/\S+@\S+\.\S+/.test(questionData.userName)) {
-       err.userNameError = 'User name should be only alphanumeric.';
-     } else if (
-       questionData.full_name.length < 2 ||
-       questionData.full_name.length > 12
-     ) {
-       err.full_nameError = 'Fullname must be between 2-12 characters.';
-     } else if (questionData.student_class === '') {
-       err.student_classError = 'Student class name is required.';
-       // } else if (/^\d*$/.test(parseInt(questionData.student_class))) {
-       //   err.student_classError = 'Student class must be number.';
-     } else if (questionData.roll_no === '') {
-       err.roll_noError = 'Roll no is required.';
-       // } else if (/^\d*$/.test(questionData.roll_no)) {
-       //   err.roll_noError = 'Roll No must be number.';
-     } else if (questionData.batch === '') {
-       err.batchError = 'batch name is required.';
-     }
-     setErrors(err);
-     if (Object.keys(err).length === 0) return true;
-     else return false;
-   };
+  const handleChangeform = () => {
+    console.log("selectedQuestion", selectedQuestion);
+    var userData = JSON.parse(window.localStorage.getItem("user"));
+    if (formErrorValidation()) {
+      questionData.questions = selectedQuestion;
+      axios
+        .post("http://3.139.234.205/add-exam/", questionData, {
+          headers: {
+            Authorization: `JWT ` + userData?.token,
+          },
+        })
+        .then((res) => {
+          alert(res?.message);
+          console.log("RESPONSE ==== : ", res);
+        })
+        .catch((err) => {
+          alert("something want to wrong");
+          console.log("ERROR: ====", err);
+        });
+    }
+  };
+  const formErrorValidation = () => {
+    questionData.questions = selectedQuestion;
+    const err = {};
+    if (questionData.title === "") {
+      err.titleError = "Title is required.";
+    } else if (questionData.assessment_date === "") {
+      err.assessment_dateError = "Assessment Date is required.";
+    } else if (questionData.exam_time === "") {
+      err.exam_timeError = "Exam Time is required.";
+    } else if (questionData.duration === "") {
+      err.durationError = "duration name is required.";
+    } else if (questionData.batch === "") {
+      err.batchError = "Batch is required.";
+    } else if (questionData.teacher === "") {
+      err.teacherError = "Teacher name is required.";
+    } else if (questionData.questions.length === 0) {
+      err.questionsError = "Atlist one Question is required.";
+    }
+    setErrors(err);
+    if (Object.keys(err).length === 0) return true;
+    else return false;
+  };
   console.log(formErrorValidation);
 
   const populateStudentList = () => {
-    var userData = JSON.parse(window.localStorage.getItem('user'));
+    var userData = JSON.parse(window.localStorage.getItem("user"));
 
     const getData = async () => {
       axios
         .post(
-          'http://3.139.234.205/get-question/',
+          "http://3.139.234.205/get-question/",
           {},
           {
             headers: {
-              Authorization: `JWT ` + userData?.token
-            }
+              Authorization: `JWT ` + userData?.token,
+            },
           }
         )
         .then((res) => {
           setQuestionList(res.data.data);
-          console.log('RESPONSE ==== : ', res);
+          console.log("RESPONSE ==== : ", res);
           // console.log('RESPONSE ==== : ', schoolList);
         })
         .catch((err) => {
-          alert('something want to wrong');
-          console.log('ERROR: ====', err);
+          alert("something want to wrong in getting a questions.");
+          console.log("ERROR: ====", err);
         });
     };
     getData();
   };
   const columns = [
     {
-      title: 'ID',
-      field: 'id'
+      title: "ID",
+      field: "id",
     },
     {
-      title: 'Question',
-      field: 'question'
+      title: "Question",
+      field: "question",
     },
     {
-      title: 'Option A',
-      field: 'option_a'
+      title: "Option A",
+      field: "option_a",
     },
     {
-      title: 'Option B',
-      field: 'option_b'
+      title: "Option B",
+      field: "option_b",
     },
     {
-      title: 'Option C',
-      field: 'option_c'
+      title: "Option C",
+      field: "option_c",
     },
     {
-      title: 'Option D',
-      field: 'option_d'
+      title: "Option D",
+      field: "option_d",
     },
     {
-      title: 'Correct Answer',
-      field: 'correct_answer'
+      title: "Correct Answer",
+      field: "correct_answer",
     },
     {
-      title: 'school ',
-      field: 'school'
+      title: "school ",
+      field: "school",
     },
     {
-      title: 'Batch',
-      field: 'batch'
-    }
+      title: "Batch",
+      field: "batch",
+    },
   ];
-   const setForm = (event) => {
-     let { name, value } = event.target;
+  const setForm = (event) => {
+    let { name, value } = event.target;
 
-     setQuestionData({
-       ...questionData,
-       [name]: value
-     });
-   };
+    setQuestionData({
+      ...questionData,
+      [name]: value,
+    });
+  };
 
   return (
     <>
@@ -248,7 +245,7 @@ export default function CreateExam() {
           <div className={classes.typo}>
             <div className={classes.note}>duration</div>
             <input
-              type="text"
+              type="number"
               name="duration"
               placeholder="Enter duration"
               onChange={(e) => setForm(e)}
@@ -301,6 +298,11 @@ export default function CreateExam() {
               )}
             </div>
           </div>
+          <div>
+            {errors.questionsError && (
+              <p className="error_productForm">{errors.questionsError}</p>
+            )}
+          </div>
           <button
             type="submit"
             className="btn btn-secondary"
@@ -322,9 +324,9 @@ export default function CreateExam() {
                   filtering: true,
                   selection: true,
                   selectionProps: (rowData) => ({
-                    disabled: rowData.name === 'Mehmet',
-                    color: 'primary'
-                  })
+                    disabled: rowData.name === "Mehmet",
+                    color: "primary",
+                  }),
                 }}
                 onSelectionChange={(rowData) =>
                   setSelectedQuestion(rowData.map((item) => item.id))
--- src/views/Exam/QuestionForm.js
+++ src/views/Exam/QuestionForm.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 // @material-ui/core components
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 // core components
 // import Quote from "components/Typography/Quote.js";
 // import Muted from "components/Typography/Muted.js";
@@ -10,45 +10,48 @@ import { makeStyles } from '@material-ui/core/styles';
 // import Success from "components/Typography/Success.js";
 // import Warning from "components/Typography/Warning.js";
 // import Danger from "components/Typography/Danger.js";
-import Card from 'components/Card/Card.js';
-import CardHeader from 'components/Card/CardHeader.js';
-import CardBody from 'components/Card/CardBody.js';
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
 
 const styles = {
   typo: {
-    paddingLeft: '25%',
-    marginBottom: '40px',
-    position: 'relative'
+    paddingLeft: "25%",
+    marginBottom: "40px",
+    position: "relative",
   },
   note: {
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-    bottom: '10px',
-    color: '#c0c1c2',
-    display: 'block',
-    fontWeight: '400',
-    fontSize: '13px',
-    lineHeight: '13px',
-    left: '0',
-    marginLeft: '20px',
-    position: 'absolute',
-    width: '260px'
+    bottom: "10px",
+    color: "#c0c1c2",
+    display: "block",
+    fontWeight: "400",
+    fontSize: "13px",
+    lineHeight: "13px",
+    left: "0",
+    marginLeft: "20px",
+    position: "absolute",
+    width: "260px",
   },
   cardCategoryWhite: {
-    color: 'rgba(255,255,255,.62)',
-    margin: '0',
-    fontSize: '14px',
-    marginTop: '0',
-    marginBottom: '0'
+    color: "rgba(255,255,255,.62)",
+    margin: "0",
+    fontSize: "14px",
+    marginTop: "0",
+    marginBottom: "0",
   },
   cardTitleWhite: {
-    color: '#FFFFFF',
-    marginTop: '0px',
-    minHeight: 'auto',
-    fontWeight: '300',
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: '3px',
-    textDecoration: 'none'
-  }
+    marginBottom: "3px",
+    textDecoration: "none",
+  },
+  select: {
+    width: "24%",
+  },
 };
 
 const useStyles = makeStyles(styles);
@@ -57,60 +60,49 @@ export default function QuestionForm() {
   const classes = useStyles();
   const [errors, setErrors] = useState({});
   const [questionData, setQuestionData] = useState({
-    username: '',
-    password: '',
-    confirm_password: '',
-    full_name: '',
-    student_class: '',
-    gender: '',
-    roll_no: '',
-    batch: ''
+    question: "",
+    option_a: "",
+    option_b: "",
+    option_c: "",
+    option_d: "",
+    correct_answer: "",
   });
 
   const handleChange = () => {
-    var userData = JSON.parse(window.localStorage.getItem('user'));
+    var userData = JSON.parse(window.localStorage.getItem("user"));
 
     console.log(questionData);
-    // if (formErrorValidation()) {
-      console.log('questionData', questionData);
+    if (formErrorValidation()) {
+      console.log("questionData", questionData);
       axios
-        .post('http://3.139.234.205/exam/add-question/', questionData, {
+        .post("http://3.139.234.205/exam/add-question/", questionData, {
           headers: {
-            Authorization: `JWT ` + userData?.token
-          }
+            Authorization: `JWT ` + userData?.token,
+          },
         })
         .then((res) => {
           alert(res?.message);
-          console.log('RESPONSE ==== : ', res);
+          console.log("RESPONSE ==== : ", res);
         })
         .catch((err) => {
-          alert('something want to wrong');
-          console.log('ERROR: ====', err);
+          alert("something want to wrong");
+          console.log("ERROR: ====", err);
         });
-    // }
+    }
   };
 
   const formErrorValidation = () => {
     const err = {};
-    if (questionData.username === '') {
-      err.usernameError = 'Username is required.';
-    } else if (/\S+@\S+\.\S+/.test(questionData.userName)) {
-      err.userNameError = 'User name should be only alphanumeric.';
-    } else if (
-      questionData.full_name.length < 2 ||
-      questionData.full_name.length > 12
-    ) {
-      err.full_nameError = 'Fullname must be between 2-12 characters.';
-    } else if (questionData.student_class === '') {
-      err.student_classError = 'Student class name is required.';
-      // } else if (/^\d*$/.test(parseInt(questionData.student_class))) {
-      //   err.student_classError = 'Student class must be number.';
-    } else if (questionData.roll_no === '') {
-      err.roll_noError = 'Roll no is required.';
-      // } else if (/^\d*$/.test(questionData.roll_no)) {
-      //   err.roll_noError = 'Roll No must be number.';
-    } else if (questionData.batch === '') {
-      err.batchError = 'batch name is required.';
+    if (questionData.question === "") {
+      err.questionError = "question is required.";
+    } else if (questionData.option_a === "") {
+      err.option_aError = "option A is required.";
+    } else if (questionData.option_b === "") {
+      err.option_bError = "option B is required.";
+    } else if (questionData.option_c === "") {
+      err.option_cError = "option C is required.";
+    } else if (questionData.option_d === "") {
+      err.option_dError = "option D is required.";
     }
     setErrors(err);
     if (Object.keys(err).length === 0) return true;
@@ -122,14 +114,14 @@ export default function QuestionForm() {
 
     setQuestionData({
       ...questionData,
-      [name]: value
+      [name]: value,
     });
   };
 
   return (
     <Card>
       <CardHeader color="primary">
-        <h4 className={classes.cardTitleWhite}>Add Student </h4>
+        <h4 className={classes.cardTitleWhite}>Add Question </h4>
         <p className={classes.cardCategoryWhite}>Fill all Fileds</p>
       </CardHeader>
       <CardBody>
@@ -204,13 +196,14 @@ export default function QuestionForm() {
           </div>
         </div>
         <div className={classes.typo}>
-          <div className={classes.note}>correct_answer</div>
-          <input
-            type="text"
-            name="correct_answer"
-            placeholder="Enter correct_answer"
-            onChange={(e) => setForm(e)}
-          />
+          <div className={classes.note}>correct Answer</div>
+
+          <select name="correct_answer" className={classes.select}>
+            <option value="a">A</option>
+            <option value="b">B</option>
+            <option value="c">C</option>
+            <option value="d">D</option>
+          </select>
           <div>
             {errors.correct_answerError && (
               <p className="error_productForm">{errors.correct_answerError}</p>
